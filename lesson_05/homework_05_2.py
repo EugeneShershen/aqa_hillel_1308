7db5ef0d0f34150346f5106e92684120808d503b
Eugene Shershen <dadn2004@gmail.com>
2024-08-31 16:54:42 +0300

    add solution to the problem statement

--- lesson_05/homework_05_2.py
+++ lesson_05/homework_05_2.py
@@ -5,19 +5,49 @@
 #   have age >=30. Print condition check result
 
 people_records = [
-  ('John', 'Doe', 28, 'Engineer', 'New York'),
-  ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
-  ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
-  ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
-  ('Michael', 'Brown', 22, 'Student', 'Seattle'),
-  ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
-  ('David', 'Miller', 33, 'Software Developer', 'Austin'),
-  ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
-  ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
-  ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
-  ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
-  ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
-  ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
-  ('Ava', 'White', 42, 'Journalist', 'San Diego'),
-  ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+    ('John', 'Doe', 28, 'Engineer', 'New York'),
+    ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+    ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+    ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+    ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+    ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+    ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+    ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+    ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+    ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+    ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+    ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+    ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+    ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+    ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
 ]
+
+# task 1
+people_records.insert(0, ('Eugene', 'Shershen', 20, 'Student', 'Odesa'))
+
+# task 2
+print('-' * 100)
+print('Before swap:\n')
+
+for people in people_records:
+    print(people)
+
+print('-' * 100)
+print('After swap:\n')
+
+people_records[1], people_records[5] = people_records[5], people_records[1]
+for people in people_records:
+    print(people)
+
+print('-' * 100)
+
+# task 3
+peoples = (people_records[6], people_records[10], people_records[13])
+
+for people in peoples:
+    if people[2] >= 30:
+        print(f'{people[0]}\'s age is >= 30')
+    else:
+        print(f'{people[0]}\'s age is not >= 30')
+
+print('-' * 100)
