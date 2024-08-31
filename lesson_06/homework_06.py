e7fa2038e2b5dbf0341f0340aaf9455920e25c5f
Eugene Shershen <dadn2004@gmail.com>
2024-08-31 21:29:39 +0300

    add function 'homework_6_2()' in 'homework_06.py'

--- lesson_06/homework_06.py
+++ lesson_06/homework_06.py
@@ -22,5 +22,22 @@ def homework_6_1():
     sep_line()
 
 
-# call a function 'homework_6_1()'
+def homework_6_2():
+    sep_line()
+
+    input_string = ''
+    in_while = True
+
+    while in_while:
+        input_string = input('Enter a word with the letter "h": ')
+        if input_string.find('h') != -1 or input_string.find('H') != -1:
+            in_while = False
+
+    print('The word you entered:', input_string)
+
+    sep_line()
+
+
+# call functions 'homework_6_1()', 'homework_6_2()'
 homework_6_1()
+homework_6_2()
