8f453fd1942e31e5f16b03a4cf1bc3a73eb826b4
Eugene Shershen <dadn2004@gmail.com>
2024-08-31 21:23:02 +0300

    add function 'homework_6_1()' in 'homework_06.py'

--- lesson_06/homework_06.py
+++ lesson_06/homework_06.py
@@ -1,3 +1,26 @@
 # separated line
 def sep_line():
     print('-' * 100)
+
+
+# homework 6.1
+def homework_6_1():
+    sep_line()
+
+    input_string = input('Enter anything: ')
+    set_string = set(input_string)
+
+    count_set_string = 0
+    for element in set_string:
+        count_set_string += 1
+
+    if count_set_string > 10:
+        print('Result: True')
+    else:
+        print('Result: False')
+
+    sep_line()
+
+
+# call a function 'homework_6_1()'
+homework_6_1()
