3b929f9b7676b067e040103de21f3d952ce3f7d3
Eugene Shershen <dadn2004@gmail.com>
2025-04-14 14:05:43 +0300

    optimized function logic

--- lesson_13/task_1/homework_13_1.py
+++ lesson_13/task_1/homework_13_1.py
@@ -13,25 +13,20 @@ def compare_for_duplicates_csv(path_file):
     """Appends unique data from given csv-file to file 'result_shershen.csv'
     :param path_file: path for a given file
     """
-    with open(path_file, newline='') as output_file:
-        reader_output_file = csv.reader(output_file)
+    result_id_list = []
+    with open(path_result_shershen, newline='') as result_file:
+        reader_result_file = csv.reader(result_file)
+        for row in reader_result_file:
+            result_id_list.append(row[0])
 
-        for row in reader_output_file:
-
-            with open(path_result_shershen, newline='') as input_file:
-                reader_input_file = csv.reader(input_file)
-                temp_list_id = []
-
-                for element in reader_input_file:
-                    list_el = list(element)
-                    temp_list_id.append(list_el[0])
-
-            if row[0] in temp_list_id:
-                pass
-            else:
-                with open(path_result_shershen, 'a', newline='') as input_file:
-                    writer_input_file = csv.writer(input_file)
-                    writer_input_file.writerow(row)
+    with open(path_file, newline='') as input_file:
+        reader_input_file = csv.reader(input_file)
+        for row in reader_input_file:
+            if row[0] not in result_id_list:
+                with open(path_result_shershen, 'a', newline='') as result_file:
+                    writer_result_file = csv.writer(result_file)
+                    writer_result_file.writerow(row)
+                    result_id_list.append(row[0])
 
 
 compare_for_duplicates_csv(path_first_file)
